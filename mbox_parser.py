70fc5debf1fdb5dd11d8fc53b50f6f4c5dd04497
dekassie10 <dekassie10@gmail.com>
2016-09-12 13:52:59 -0700

    Add files via upload
    more_payloads gets the message body
    date_splitter currently just splits the day (Mon, Tue, Wed..) from the date
    in main message has a bunch of headers to display.  Only a handful of them are displayed

---
+++ mbox_parser.py
@@ -0,0 +1,27 @@
+import mailbox
+import csv
+import sys
+
+def more_payloads(message):
+	body = ""
+	if message.is_multipart():
+		for payload in message.get_payload():
+			body += more_payloads(payload)
+	else:
+		if message.get_content_type() == 'text/plain':
+			body = message.get_payload(decode=True)
+	return body
+def date_splitter(message):
+    if message.get_all('date'):
+        message_string = ''.join(message.get_all('date'))
+        splits = message_string.split(",")
+        newline = []
+        day = ""
+        newline.append(splits)
+    return newline
+
+writer = csv.writer(open("clean_mail.csv", "wb"))
+for message in mailbox.mbox('mailbox2.mbox'):
+        body = more_payloads(message)
+        date = date_splitter(message)
+	writer.writerow([message['subject'], message['from'], date[0][0], date[0][1], message['to'],message['x-gmail-labels'],message['x-autoreply'],body])
